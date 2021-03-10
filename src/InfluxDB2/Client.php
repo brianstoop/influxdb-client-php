dac14f1963b4a3e71f539a97e97045a17f8b6e61
Sean Molenaar <sean@m2mobi.com>
2021-03-10 13:11:12 +0100

    chore: Hide api details from function names

--- src/InfluxDB2/Client.php
+++ src/InfluxDB2/Client.php
@@ -23,8 +23,7 @@ class Client
     public $options;
     public $closed = false;
     private $autoCloseable = array();
-    private $curl_api;
-    private $guzzle_api;
+    private $api = NULL;
 
     /**
      * Client constructor.
@@ -68,31 +67,18 @@ class Client
         $this->options = $options;
     }
 
-    private function getGuzzleApi(): GuzzleApi
+    public function setApi(DefaultApi $api): void
     {
-        return $this->guzzle_api ?? new GuzzleApi($this->options);
+        $this->api = $api;
     }
 
-    private function getCurlApi(): CurlApi
+    public function getApi(): DefaultApi
     {
-        return $this->curl_api ?? new CurlApi($this->options);
-    }
+        if ($this->api === NULL) {
+            $this->api = new GuzzleApi($this->options);
+        }
 
-    /**
-     * Write time series data into InfluxDB through the WriteApi.
-     *      $writeOptions = [
-     *          'writeType' => methods of write (WriteType::SYNCHRONOUS - default, WriteType::BATCHING)
-     *          'batchSize' => the number of data point to collect in batch
-     *      ]
-     * @param array|null $writeOptions Array containing the write parameters (See above)
-     * @param array|null $pointSettings Array of default tags
-     * @return WriteApi
-     */
-    public function createWriteApi(array $writeOptions = null, array $pointSettings = null): WriteApi
-    {
-        $writeApi = new WriteApi($this->options, $writeOptions, $pointSettings, $this->getGuzzleApi());
-        $this->autoCloseable[] = $writeApi;
-        return $writeApi;
+        return $this->api;
     }
 
     /**
@@ -105,9 +91,9 @@ class Client
      * @param array|null $pointSettings Array of default tags
      * @return WriteApi
      */
-    public function createCurlWriteApi(array $writeOptions = null, array $pointSettings = null): WriteApi
+    public function createWriteApi(array $writeOptions = null, array $pointSettings = null): WriteApi
     {
-        $writeApi = new WriteApi($this->options, $writeOptions, $pointSettings, $this->getCurlApi());
+        $writeApi = new WriteApi($this->options, $writeOptions, $pointSettings, $this->getApi());
         $this->autoCloseable[] = $writeApi;
         return $writeApi;
     }
@@ -128,17 +114,7 @@ class Client
      */
     public function createQueryApi(): QueryApi
     {
-        return new QueryApi($this->options, $this->getGuzzleApi());
-    }
-
-    /**
-     * Get the Query client.
-     *
-     * @return QueryApi
-     */
-    public function createCurlQueryApi(): QueryApi
-    {
-        return new QueryApi($this->options, $this->getCurlApi());
+        return new QueryApi($this->options, $this->getApi());
     }
 
     /**
@@ -149,17 +125,7 @@ class Client
      */
     public function health(): HealthCheck
     {
-        return (new HealthApi($this->getGuzzleApi()))->health();
-    }
-
-    /**
-     * Get the health of an instance.
-     *
-     * @return HealthCheck
-     */
-    public function curlHealth(): HealthCheck
-    {
-        return (new HealthApi($this->getCurlApi()))->health();
+        return (new HealthApi($this->getApi()))->health();
     }
 
     /**
@@ -201,19 +167,12 @@ class Client
      * @param  $serviceClass
      * @return object service instance
      */
-    public function createCurlService($serviceClass)
-    {
-        throw new RuntimeException('cURL services are unsupported');
-    }
-
-    /**
-     * Creates the instance of api service
-     *
-     * @param  $serviceClass
-     * @return object service instance
-     */
     public function createService($serviceClass)
     {
+        if (!($this->getApi() instanceof GuzzleApi)) {
+            throw new RuntimeException('Only Guzzle API is supported!');
+        }
+
         try {
             $class = new ReflectionClass($serviceClass);
             $args = array($this->getGuzzleClient(), $this->getConfiguration());
@@ -225,6 +184,6 @@ class Client
 
     private function getGuzzleClient()
     {
-        return $this->getGuzzleApi()->http;
+        return (new GuzzleApi($this->options))->http;
     }
 }
